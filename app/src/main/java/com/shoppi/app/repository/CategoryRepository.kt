19147dabf468d42b99d315e66ec4480193073fac
dorian.kim <dorian21@naver.com>
2023-11-02 22:08:15 +0900

    [섹선 9] add Coroutine

---
+++ app/src/main/java/com/shoppi/app/repository/CategoryRepository.kt
@@ -0,0 +1,12 @@
+package com.shoppi.app.repository
+
+import com.shoppi.app.model.Category
+
+class CategoryRepository(
+    private val remoteDataSource: CategoryRemoteDataSource
+) {
+
+    suspend fun getCategories(): List<Category> {
+        return remoteDataSource.getCategories()
+    }
+}
