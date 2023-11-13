5e03c39d97962cdcdf6abed00c6ce09a4d909f99
dorian.kim <dorian21@naver.com>
2023-11-13 21:20:57 +0900

    add nested top selling

--- app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapters.kt
@@ -11,5 +11,14 @@ fun loadImage(view: ImageView, imageUrl: String?){
             .load(imageUrl)
             .into(view)
     }
+}
 
+@BindingAdapter("circleImageUrl")
+fun loadCircleImage(view: ImageView, imageUrl: String?){
+    if (!imageUrl.isNullOrEmpty()){
+        GlideApp.with(view)
+            .load(imageUrl)
+            .circleCrop()
+            .into(view)
+    }
 }
